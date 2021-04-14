af8804fb019814bef533f7991386e277f06bb4ff
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-04-14 18:00:02 +0300

    first commit

---
+++ src/main/java/ru/netology/Person.java
@@ -0,0 +1,49 @@
+package ru.netology;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public boolean hasAge() {
+        
+    }
+
+    public boolean hasAddress() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+
+}
